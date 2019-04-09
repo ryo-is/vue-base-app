05b327dd071f043bc183c0c0e9a0616c2d6ba9f9
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-09 15:42:32 +0900

    fix:change tslint rule (semicolon)

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,12 @@
-import { shallowMount, Wrapper } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount, Wrapper } from "@vue/test-utils"
+import HelloWorld from "@/components/HelloWorld.vue"
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg: string = "new message";
+    const msg: string = "new message"
     const wrapper: Wrapper<HelloWorld> = shallowMount(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch(msg)
   });
 });

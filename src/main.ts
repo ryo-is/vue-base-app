0d04e00ace361c585cd0edb8c1162fcb2f9dc45a
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-10 18:04:22 +0900

    feat:Amplifyをインストール

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import "./registerServiceWorker"
 import "./plugins/element.js"
 
-Vue.config.productionTip = false;
+import Amplify from "aws-amplify"
+import awsExports from "@/aws-exports.js"
+Amplify.configure(awsExports)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
--- src/views/home/home.ts
+++ src/views/home/home.ts
@@ -1,12 +1,7 @@
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/helloWorld/HelloWorld.vue"; // @ is an alias to /src
 import router from "@/router";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component({})
 export default class Home extends Vue {
   public title: string = "Vue Vote App"
 
